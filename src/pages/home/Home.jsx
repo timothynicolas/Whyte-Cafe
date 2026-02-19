cb5a0b2e788d3f67ce414d07c5b42a5e2174eab1
Timothy Nicolas <timothynicolas24@gmail.com>
2026-02-19 23:09:02 +0800

    03: Version before uninstalling tailwindcss on local repo.f it we go raw

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,10 +3,9 @@ import { Hero } from "./Hero";
 
 export function Home() {
   return (
-    <div className="px-9 py-4">
+    <div className="px-8 py-4 sm:px-16 md:px-20 xl:px-25 2xl:px-30 flex flex-col ">
       <Navbar />
-      <Hero />
+      {/* <Hero /> */}
     </div>
-    
   );
 }
