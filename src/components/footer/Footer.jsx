6680785d599419b893112bf4d4703146b224811b
Timothy Nicolas <timothynicolas24@gmail.com>
2026-04-23 19:54:09 +0800

    19: Added social links to Footer icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,15 +3,25 @@ import facebook from "../../assets/footer/facebook.svg";
 import instagram from "../../assets/footer/instagram.svg";
 import email from "../../assets/footer/email.svg";
 
-export function Footer(){
-  return(
+export function Footer() {
+  return (
     <div className={styles.container}>
-      <hr className={styles.hr}/>
+      <hr className={styles.hr} />
       <p className={styles.footerText}>© 2026 Whyte Cafe, Philippines</p>
       <div className={styles.socialLinkContainer}>
-        <img className={styles.socialLink} src={facebook}/>
-        <img className={styles.socialLink} src={instagram}/>
-        <img className={styles.socialLink} src={email}/>
+        <a href="https://www.facebook.com/profile.php?id=61574877994032">
+          <img className={styles.socialLink} src={facebook} />
+        </a>
+        <a href="https://www.instagram.com/thewhytecafe/">
+          <img className={styles.socialLink} src={instagram} />
+        </a>
+        <a
+          href="https://mail.google.com/mail/?view=cm&to=whytecafe@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className={styles.socialLink} src={email} />
+        </a>
       </div>
     </div>
   );
